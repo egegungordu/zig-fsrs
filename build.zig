7a01aa6bb44d7ff8e7465b58316dea3e0c77a37d
Ege Gungordu <egegungorduu@hotmail.com>
2024-07-09 02:42:38 +0300

    misc: add simple example

--- build.zig
+++ build.zig
@@ -1,16 +1,35 @@
 const std = @import("std");
 
-pub fn build(b: *std.Build) void {
+pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
 
     const optimize = b.standardOptimizeOption(.{});
 
+    // Module
     const zig_fsrs_module = b.addModule("zig-fsrs", .{
         .root_source_file = b.path("src/zig-fsrs.zig"),
         .target = target,
         .optimize = optimize,
     });
 
+    // Examples
+    const Example = enum { simple };
+    const example_option = b.option(Example, "example", "Example to run (default: simple)") orelse .simple;
+    const example_step = b.step("example", "Run example");
+    const example = b.addExecutable(.{
+        .name = "example",
+        .root_source_file = b.path(
+            b.fmt("examples/{s}.zig", .{@tagName(example_option)}),
+        ),
+        .target = target,
+        .optimize = optimize,
+    });
+    example.root_module.addImport("zig-fsrs", zig_fsrs_module);
+
+    const example_run = b.addRunArtifact(example);
+    example_step.dependOn(&example_run.step);
+
+    // Tests
     const tests = b.addTest(.{
         .root_source_file = b.path("src/tests.zig"),
         .target = target,
--- examples/simple.zig
+++ examples/simple.zig
@@ -1,9 +1,3 @@
-# zig-fsrs
-
-This is an implementation of [FSRS-4.5](https://github.com/open-spaced-repetition/fsrs4anki/wiki/The-Algorithm) in Zig.  
-This project is on zig 0.13.0
-
-```zig
 const std = @import("std");
 const fsrs = @import("zig-fsrs");
 const Card = fsrs.Card;
@@ -45,43 +39,3 @@ fn print_card(card: *const Card, title: []const u8) void {
     std.debug.print("  scheduled days: {d}\n", .{card.scheduled_days});
     std.debug.print("  due: {d}\n", .{card.due});
 }
-```
-
-## Getting Started
-
-```shell
-git clone https://github.com/egegungordu/zig-fsrs.git
-cd zig-fsrs
-zig build test --summary all
-```
-
-If you want to include zig-fsrs in your own zig project:
-
-Fetch zig-fsrs:
-
-```shell
-zig fetch --save git+https://github.com/egegungordu/zig-fsrs
-```
-
-Add zig-fsrs to your `build.zig` file:
-
-```zig
-const zig_fsrs = b.dependency("zig-fsrs", .{});
-exe.root_module.addImport("zig-fsrs", zig_fractions.module("zig-fsrs"));
-```
-
-Now you can import zig-fsrs in your code:
-
-```zig
-const fsrs = @import("zig-fsrs");
-```
-
-## Usage
-
-To run the examples:
-
-```shell
-zig build example -Dexample=example_name
-```
-
-
